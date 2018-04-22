3126b84d8a903b4c7c6a90c806e8f1c69a10c84b
Mario Meissner <mr.rash.boy@gmail.com>
2018-04-22 13:44:46 +0200

    Add course_list page skeleton

---
+++ prototype/lib/pages/course_list.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+import 'package:prototype/ui/widgets/line_separator.dart';
+
+class CourseList extends StatelessWidget{
+
+  @override
+  build(BuildContext context){
+    return new ListView(
+
+    );
+  }
+}
