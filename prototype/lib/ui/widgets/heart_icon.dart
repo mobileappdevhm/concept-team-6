dd857d5913fdbce92a63cfee758b2181afc8d191
Mario Meissner <mr.rash.boy@gmail.com>
2018-04-22 16:29:37 +0200

    Add heart icon skeleton

---
+++ prototype/lib/ui/widgets/heart_icon.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HeartIcon extends StatelessWidget{
+
+  final bool _marked;
+  HeartIcon(this._marked);
+
+  @override
+  Widget build(BuildContext context){
+    return new Container();
+  }
+}
