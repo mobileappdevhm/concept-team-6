110c8d1aead830f2f681b5e770a7cfa51ef18ec5
Mario Meissner <mr.rash.boy@gmail.com>
2018-04-22 12:48:10 +0200

    Add line separator widget

---
+++ prototype/lib/ui/widgets/line_separator.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+
+class LineSeparator extends StatelessWidget {
+
+  String _text;
+
+  LineSeparator(this._text);
+
+  @override build(BuildContext context){
+    
+    /* TODO: How to represent the line? */
+
+    return new Container(
+      //padding: new EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+      //child: new new RRect.fromLTRBAndCorners(-10.0, -11.0, 10.0, 11.0)
+    );
+
+  }
+}
